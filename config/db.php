97cac7f8a648e51cab0d1a52e876dbc8e3652dac
Jesus Rios <jesus.rios.carrera@estudiante.uacm.edu.mx>
2026-04-14 00:40:58 -0600

    Configuracion de la conexion a la base de datos en l aimagen de docker + configuracion del archivo Dockerfile

--- config/db.php
+++ config/db.php
@@ -1,19 +1,18 @@
 <?php
+require_once __DIR__ . '/config.php';
+
 class DatabasePDO {
-    private $host = "localhost";
-    private $db_name = "sistema_comanda_digital_v1";
-    private $username = "root"; // cambia si tienes otro usuario
-    private $password = "";     // cambia si tienes contraseña
     public $conn;
 
     public function getConnection() {
         $this->conn = null;
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, 
-                                  $this->username, $this->password);
+            $this->conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, 
+                                  DB_USER, DB_PASS);
             $this->conn->exec("set names utf8");
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $exception) {
-            echo "Error de conexión: " . $exception->getMessage();
+            echo "Error de conexión PDO: " . $exception->getMessage();
         }
         return $this->conn;
     }
