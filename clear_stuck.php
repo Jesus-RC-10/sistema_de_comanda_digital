659c2923b049ee30a7a0841c03eab079e456d07c
Jesus Rios <jesus.rios.carrera@estudiante.uacm.edu.mx>
2026-05-26 01:57:16 -0600

    Eliminacion de archivos basura.

--- clear_stuck.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dsn = "mysql:host=127.0.0.1;port=3307;dbname=sistema_comanda_digital_v1;charset=utf8mb4";
-$db = new PDO($dsn, 'root', 'root');
-$db->exec("UPDATE pedidos SET estado='entregado'");
-$db->exec("UPDATE pedido_detalles SET estado='entregado'");
-$db->exec("UPDATE ventas SET estado='pagado'");
-echo 'OK';
